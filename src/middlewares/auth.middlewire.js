f14fbc773e1422a42e8d4d990aebff4cb558fdee
pritam-mb <patrapritam567@gmail.com>
2025-10-23 23:44:08 +0530

    small bugs

--- src/middlewares/auth.middlewire.js
+++ src/middlewares/auth.middlewire.js
@@ -3,6 +3,6 @@ import { asyncHandler } from "../utils/asyncHandler";
 export const verifyjwt = asyncHandler(async(req,res,next)=>{
     const token = req.cookies.accessToken || req.headers.authorization?.replace("Bearer ")[1]
     if(!token){
-        throw new apierror("unauthenticated access",401)
+        throw new apierror("unauthenticated access",401) // custom error class
     }
 })
