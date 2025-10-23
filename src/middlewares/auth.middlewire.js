c9fa87e6be4936056074d3197a6859d0a105b69a
pritam-mb <patrapritam567@gmail.com>
2025-10-24 02:45:41 +0530

    identation

--- src/middlewares/auth.middlewire.js
+++ src/middlewares/auth.middlewire.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler";
 
 export const verifyjwt = asyncHandler(async(req,res,next)=>{
-    const token = req.cookies.accessToken || req.headers.authorization?.replace("Bearer ")[1]
+    const token = req.cookies.accessToken || req.headers.authorization?.replace("Bearer ")[1] // getting token from cookies or headers
     if(!token){
         throw new apierror("unauthenticated access",401) // custom error class
     }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,60 +2,7 @@ import mongoose,{Schema} from "mongoose";
 // import { use } from "react";
 import jwt from "jsonwebtoken" // for generating token
 import bcrypt from "bcrypt"  // for hashing password or encrypting password
-const userSchema = new Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true  // for optimise searching techniqs
-    }
-    ,
-     email:{
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        // index: true  // for optimise searching techniqs
-    },
-     fullname:{
-        type: String,
-        required: true,
-        // unique: true,
-        // lowercase: true,
-        trim: true,
-        index: true  // for optimise searching techniqs
-    },
-    avatar:{
-        type: String,
-        required: true
-    },
-    coverImage:{
-        type: String,
-        // required: true 
-    },
-    watchHistory:[
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Videos"
-        }
-    ],
-    password:{
-        type: String,
-        required: [true, 'password is required']
-
-    },
-    refreshToken:{
-        type: String
-    }
-
-
-},{
-    timestamps:true
-})
-
+ 
 //encrypt password before storing
 userSchema.pre("save", async function (next) {  // next is just passing the control to next function
     if(! this.isModified("password")) return next()
